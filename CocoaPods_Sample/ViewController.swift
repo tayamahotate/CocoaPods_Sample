9c34bd82093e3b1235862c1e81483c9f4c968cb8
hotatekaoru <taymahotate@gmail.com>
2015-10-08 00:10:16 +0900

    Initial Commit

---
+++ CocoaPods_Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CocoaPods_Sample
+//
+//  Created by 田山由理 on 2015/10/08.
+//  Copyright (c) 2015年 yuri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
